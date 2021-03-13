5c75099e1a873e75d8d2ba9e808460d869e604d1
Creator William <creator_william@icloud.com>
2021-03-13 20:48:55 +0800

    整合ImageKit、NetworkKit、LayoutKit、JSONKit

--- Package.swift
+++ Package.swift
@@ -11,25 +11,28 @@ let package = Package(
       name: "ProjectKit",
       type: .static,
       targets: ["ApplicationKit",
-                "ComponentKit"]),
+                "ComponentKit",
+                "LayoutKit",
+                "JSONKit",
+                "NetworkKit"]),
   ],
   dependencies: [
     // Dependencies declare other packages that this package depends on.
-    .package(url: "https://github.com/CreatorWilliam/JSONKit.git", .branch("master")),
-    .package(url: "https://github.com/CreatorWilliam/LayoutKit.git", .branch("master")),
-    .package(url: "https://github.com/CreatorWilliam/NetworkKit.git", .branch("master")),
-    .package(url: "https://github.com/CreatorWilliam/ImageKit.git", .branch("master"))
   ],
   targets: [
     // Targets are the basic building blocks of a package. A target can define a module or a test suite.
     // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    .target(
-      name: "ApplicationKit",
-      dependencies: ["LayoutKit",
-                     "JSONKit",
-                     "NetworkKit"]),
+    .target(name: "ApplicationKit",
+            dependencies: ["LayoutKit",
+                           "JSONKit",
+                           "NetworkKit"]),
     .target(name: "ComponentKit",
-            dependencies: ["ApplicationKit", "ImageKit"]),
+            dependencies: ["ApplicationKit",
+                           "ImageKit"]),
+    .target(name: "LayoutKit"),
+    .target(name: "JSONKit"),
+    .target(name: "NetworkKit"),
+    .target(name: "ImageKit"),
     .testTarget(
       name: "ApplicationKitTests",
       dependencies: ["ApplicationKit"]),
